ac7cc2ba34aa3b6147bf34698a5e5a1653a28bb8
OAM103 <113307610+OAM103@users.noreply.github.com>
2025-10-21 10:06:27 +0300

    Add files via upload

---
+++ Ping Pong Game/src/main.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include "uart_lib.h"
+#include "ball.h"
+#include "system_config.h"
+#include "lcd.h"
+#include "touchpad.h"
+#include "button.h"
+
+int main() {
+    SystemClock_Config();
+    SPI0_Init();
+    SPI1_Init();
+    GPIO_Init();
+    Lcd_Init();
+    DMA_Init();
+
+    ClearMass(480, 320, 0, 0, 0xAAA0);
+
+    Platform *plat = &right_platform;
+    while (1) {
+        //process_touchpad();
+        process_platform_touch(0x0000, 0xAAA0);
+        update_ball(0x0000, 0xAAA0, plat);
+    }
+}
