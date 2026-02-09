6c68f86306c4abfd75910e254d975c005c8d89ac
OAM103 <113307610+OAM103@users.noreply.github.com>
2026-02-09 11:17:10 +0300

    Add files via upload

---
+++ AUX_melody/src/music.h
@@ -0,0 +1,14 @@
+#ifndef MUSIC_H
+#define MUSIC_H
+#include "inttypes.h"
+#include <stdlib.h>
+
+extern const uint8_t what[];
+extern const uint8_t jump[];
+extern const uint8_t noize[];
+
+#define WHAT_LEN  224000
+#define JUMP_LEN  211320
+#define NOIZE_LEN 523364
+
+#endif
